04fc207e172ce6f04205ce39fcef82d1b312d446
布衣男儿 <iTBoyer@qq.com>
2020-06-02 13:33:08 +0800

    更新版本

--- Package.swift
+++ Package.swift
@@ -5,12 +5,12 @@ let package = Package(
 	name: "PerfectTurnstileSQLite",
 	targets: [],
 	dependencies: [
-		.Package(url: "https://github.com/SwiftORM/SQLite-StORM.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 3),
-		.Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 2),
-		.Package(url: "https://github.com/iamjono/SwiftRandom.git", majorVersion: 0),
-		.Package(url: "https://github.com/PerfectSideRepos/Turnstile-Perfect.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", majorVersion: 3),
+		.Package(url: "https://github.com/SwiftORM/SQLite-StORM.git", from: "3.0.0"),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", from: "3.0.0"),
+		.Package(url: "https://github.com/iamjono/SwiftString.git", from: "3.0.0"),
+		.Package(url: "https://github.com/iamjono/SwiftRandom.git", from: "0.0.0"),
+		.Package(url: "https://github.com/PerfectSideRepos/Turnstile-Perfect.git", from: "3.0.0"),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", from: "3.0.0"),
 	]
 )
